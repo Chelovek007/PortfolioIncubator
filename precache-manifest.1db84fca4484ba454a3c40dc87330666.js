df70bf839ed25ae13688b2c790f72d2c09f394be
Budko Ilya(team_4) <47296483+Chelovek007@users.noreply.github.com>
2019-10-15 21:30:28 +0300

    Updates

---
+++ precache-manifest.1db84fca4484ba454a3c40dc87330666.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca486fe187c9d63e3f8b8c6248ab8fc0",
+    "url": "/PortfolioIncubator/index.html"
+  },
+  {
+    "revision": "75e3771ca0fe0533436a",
+    "url": "/PortfolioIncubator/static/css/main.f27f999d.chunk.css"
+  },
+  {
+    "revision": "3bd9f1cd2ef2c40be154",
+    "url": "/PortfolioIncubator/static/js/2.5c18e7f3.chunk.js"
+  },
+  {
+    "revision": "75e3771ca0fe0533436a",
+    "url": "/PortfolioIncubator/static/js/main.cf8b0882.chunk.js"
+  },
+  {
+    "revision": "4faceb94b8c65a902fea",
+    "url": "/PortfolioIncubator/static/js/runtime~main.71be84ed.js"
+  },
+  {
+    "revision": "bdd8b44aa5867d038c1000e19ab74586",
+    "url": "/PortfolioIncubator/static/media/html-icon.bdd8b44a.svg"
+  },
+  {
+    "revision": "46b346c9daabb112e6a651f60cdeba03",
+    "url": "/PortfolioIncubator/static/media/js-icon.46b346c9.svg"
+  },
+  {
+    "revision": "44759af1e805d8eae2b1e3072458c043",
+    "url": "/PortfolioIncubator/static/media/portfolio.44759af1.jpg"
+  },
+  {
+    "revision": "151e1a817730225dd372a121109cf80e",
+    "url": "/PortfolioIncubator/static/media/react-icon.151e1a81.svg"
+  },
+  {
+    "revision": "2955967c57458a2518491a5461804aa0",
+    "url": "/PortfolioIncubator/static/media/social-network.2955967c.jpg"
+  },
+  {
+    "revision": "9ca51d2ac295d3538a2173763f5c7e70",
+    "url": "/PortfolioIncubator/static/media/telegram2.9ca51d2a.svg"
+  },
+  {
+    "revision": "0570caf12588f65bf6e85780ea23a436",
+    "url": "/PortfolioIncubator/static/media/todolist.0570caf1.jpg"
+  },
+  {
+    "revision": "8c9399e31ce755970497937f81573e54",
+    "url": "/PortfolioIncubator/static/media/vk.8c9399e3.svg"
+  },
+  {
+    "revision": "c64da20e3b7c8f88f2f32aacff04a352",
+    "url": "/PortfolioIncubator/static/media/whatsapp.c64da20e.svg"
+  }
+]);
